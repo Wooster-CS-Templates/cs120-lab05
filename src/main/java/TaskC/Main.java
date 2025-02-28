089951d659e9feaabd40babb1d5b8bb62a59d373
Drew Guarnera <dguarnera@wooster.edu>
2025-02-28 12:52:42 -0500

    Update Main.java

--- src/main/java/TaskC/Main.java
+++ src/main/java/TaskC/Main.java
@@ -7,33 +7,33 @@ public class Main {
 
     public void runCode(){
 
-        List l1 = new List();
-        //List l2 = new List();
+        // List l1 = new List();
+        // List l2 = new List();
 
-        l1.add(9);
-        l1.add(4);
-        l1.add(3);
-        l1.add(8);
-        l1.add(5);
-        l1.add(7);
-        l1.add(6);
-        l1.display();
-//        l1.find(8);
-//        l1.find(4);
-//        l1.find(1);
-//        l1.find(0);
-//
-//        l2.add(3);
-//        l2.add(4);
-//        l2.add(5);
-//        l2.add(6);
-//        l2.add(7);
-//        l2.add(8);
-//        l2.add(9);
-//        l2.display();
-//        l2.find(8);
-//        l2.find(4);
-//        l2.find(1);
-//        l2.find(0);
+        // l1.add(9);
+        // l1.add(4);
+        // l1.add(3);
+        // l1.add(8);
+        // l1.add(5);
+        // l1.add(7);
+        // l1.add(6);
+        // l1.display();
+        // l1.find(8);
+        // l1.find(4);
+        // l1.find(1);
+        // l1.find(0);
+
+        // l2.add(3);
+        // l2.add(4);
+        // l2.add(5);
+        // l2.add(6);
+        // l2.add(7);
+        // l2.add(8);
+        // l2.add(9);
+        // l2.display();
+        // l2.find(8);
+        // l2.find(4);
+        // l2.find(1);
+        // l2.find(0);
     }
 }
