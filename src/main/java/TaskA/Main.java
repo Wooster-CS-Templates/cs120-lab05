262323e30b85bd9fe5a6d4952dac82c28c945b65
Drew Guarnera <dguarnera@wooster.edu>
2025-02-28 12:51:57 -0500

    Update Main.java

--- src/main/java/TaskA/Main.java
+++ src/main/java/TaskA/Main.java
@@ -7,33 +7,33 @@ public class Main {
 
     public void runCode(){
 
-        List l1 = new List();
-        l1.add(-20);
-        l1.add(40);
-        l1.add(-7);
-        l1.add(-43);
-        l1.add(-28);
-        l1.add(37);
-        l1.add(12);
-        l1.add(-37);
-        l1.add(0);
-        l1.add(-7);
+        // List l1 = new List();
+        // l1.add(-20);
+        // l1.add(40);
+        // l1.add(-7);
+        // l1.add(-43);
+        // l1.add(-28);
+        // l1.add(37);
+        // l1.add(12);
+        // l1.add(-37);
+        // l1.add(0);
+        // l1.add(-7);
 
-        l1.display();
-//        l1.removeDups();
-//        l1.display();
-//        l1.halveEven();
-//        l1.display();
-//        l1.abs();
-//        l1.display();
-//        l1.removeDups();
-//        l1.display();
-//        l1.sub25();
-//        l1.display();
-//        l1.abs();
-//        l1.display();
-//        l1.removeDups();
-//        l1.display();
+       // l1.display();
+       // l1.removeDups();
+       // l1.display();
+       // l1.halveEven();
+       // l1.display();
+       // l1.abs();
+       // l1.display();
+       // l1.removeDups();
+       // l1.display();
+       // l1.sub25();
+       // l1.display();
+       // l1.abs();
+       // l1.display();
+       // l1.removeDups();
+       // l1.display();
 
     }
 }
